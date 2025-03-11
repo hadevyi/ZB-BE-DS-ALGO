126f025370155100dcf1e6f5c4c283c21c2a120b
hadevyi <hadevyi@gmail.com>
2025-03-12 06:47:31 +0900

    #16 Chapter14. recutson vs iterative

--- src/chapter14_Recursion_Function/Recursion03_Fibonacci.java
+++ src/chapter14_Recursion_Function/Recursion03_Fibonacci.java
@@ -1,12 +1,25 @@
 package chapter14_Recursion_Function;
 
 public class Recursion03_Fibonacci {
-    public static int fibonacci(int n) {
-        if (n <= 1) return n; // 종료 조건(Base Case)
-        return fibonacci(n - 1) + fibonacci(n - 2); // 재귀 호출
+    public static int fibonacciRecursive(int n) {
+        if (n <= 1) return n;
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+    }
+
+    public static int fibonacciIterative(int n) {
+        if (n <= 1) return n;
+
+        int prev = 0, curr = 1;
+        for (int i = 2; i <= n; i++) {
+            int next = prev + curr;
+            prev = curr;
+            curr = next;
+        }
+        return curr;
     }
 
     public static void main(String[] args) {
-        System.out.println(fibonacci(5)); // 출력: 5
+        System.out.println(fibonacciIterative(5));
+        System.out.println(fibonacciRecursive(5));
     }
 }
